01a80b18853e17967d9c8675dab0fbe55c2c4f82
Mario Resa <80423737+Mario999X@users.noreply.github.com>
2022-12-22 14:32:46 +0100

    Update StarUnix.kt

--- src/main/kotlin/db/StarUnix.kt
+++ src/main/kotlin/db/StarUnix.kt
@@ -61,8 +61,8 @@ class StarUnix {
             lector.incrementAndGet()
             println("Obteniendo misiles")
 
-            depositarNaves.signalAll()
             lector.decrementAndGet()
+            depositarNaves.signalAll()
             return misiles.toInt()
         }
     }
@@ -75,8 +75,8 @@ class StarUnix {
             lector.incrementAndGet()
             println("Obteniendo total de naves")
 
-            depositarNaves.signalAll()
             lector.decrementAndGet()
+            depositarNaves.signalAll()
             return registros.size
         }
     }
